256b482a3f37d9465f9b5057c49960f4ff1a4547
Jeremy Musser <jemusser@iu.edu>
2022-07-28 17:13:02 -0400

    Created repository with initial codebase

---
+++ smtp/main.c
@@ -0,0 +1,13 @@
+int connect_smtp(const char* host, int port);
+char* send_smtp(int sock, const char* msg);
+
+
+/*
+  Use the provided 'connect_smtp' and 'send_smtp' functions
+  to connect to the "lunar.open.sice.indian.edu" smtp relay
+  and send the commands to write emails as described in the
+  assignment wiki.
+ */
+int main(int argc, char* argv[]) {
+  return 0;
+}
