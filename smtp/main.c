2682cc193481298ca7070291e78d0aa5c9db692d
Mana Agrawal <manaagra@silo.luddy.indiana.edu>
2022-09-09 04:53:16 -0400

    initial change

--- smtp/main.c
+++ smtp/main.c
@@ -20,12 +20,13 @@ int main(int argc, char* argv[]) {
 
   char* rcpt = argv[1];
   char* filepath = argv[2];
-
+  char* data = "HELO iu.edu\n";
 
   int socket = connect_smtp("lunar.open.sice.indiana.edu", 25);
   char response[4096];
-  send_smtp(socket, "Helo iu.edu\n",rersponse,4096);
+  send_smtp(socket, data,response,4096);
   printf("%s\n", response);
   return 0;
 }
+
 
