c877aca851a820919960e6631c7ece31ce70638a
manaagra <manaagra@iu.edu>
2022-10-16 20:32:55 +0000

    address translation

--- netster/chat.c
+++ netster/chat.c
@@ -1,3 +1,6 @@
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <string.h>
 #include <stdbool.h>
 #include <sys/socket.h>
@@ -67,6 +70,44 @@ void chat_server(char* iface, long port, int use_udp) {
    int len = sizeof(client_addr);
   socklen_t addr_size;
  
+
+//here
+struct addrinfo hints, *result;
+  char addr[100];
+  char str[256];
+  sprintf(str,"%ld",port);
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = PF_UNSPEC;
+  if(use_udp == 0) {
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+ }
+  else {
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_protocol = IPPROTO_UDP;
+  }
+  hints.ai_flags = AI_PASSIVE;
+  int err = getaddrinfo(iface, str, &hints, &result );
+  if(err!= 0) {
+        perror("getaddrinfo");
+  }
+ //while(result) {
+  inet_ntop(result->ai_family, result->ai_addr->sa_data,addr,100);
+  void* raw_addr;
+if (result->ai_family == AF_INET) { // Address is IPv4
+  struct sockaddr_in* tmp = (struct sockaddr_in*)result->ai_addr; // Cast addr into AF_INET container
+  raw_addr = &(tmp->sin_addr); // Extract the address from the container
+}
+else { // Address is IPv6
+  struct sockaddr_in6* tmp = (struct sockaddr_in6*)result->ai_addr; // Cast addr into AF_INET6 container
+  raw_addr = &(tmp->sin6_addr); // Extract the address from the container
+}
+inet_ntop(result->ai_family, raw_addr, addr, 100);
+//printf("IPv%d %s\n", result->ai_family == AF_INET6?6:4, addr);
+//result = result->ai_next;
+//}
+//here
+
   if(use_udp == 0)
 	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
   else
@@ -76,7 +117,7 @@ void chat_server(char* iface, long port, int use_udp) {
  
   serverAddr.sin_port = htons(port);
 
-  serverAddr.sin_addr.s_addr = inet_addr(iface);
+  serverAddr.sin_addr.s_addr = inet_addr(addr);
 
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
  
@@ -141,18 +182,57 @@ void chat_client(char* host, long port, int use_udp) {
     int socket_c;
     struct sockaddr_in server_addr;
     char serverMessage[1024], clientMessage[1024];
+
+//here
+struct addrinfo hints, *result;
+  char addr[100];
+  char str[256];
+  sprintf(str,"%ld",port);
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = PF_UNSPEC;
+  if(use_udp == 0) {
+  	hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+ }
+  else {
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+  }
+  hints.ai_flags = AI_PASSIVE;
+  int err = getaddrinfo(host, str, &hints, &result );
+  if(err!= 0) {
+  	perror("getaddrinfo");
+  }
+ //while(result) {
+  inet_ntop(result->ai_family, result->ai_addr->sa_data,addr,100);
+  void* raw_addr;
+if (result->ai_family == AF_INET) { // Address is IPv4
+  struct sockaddr_in* tmp = (struct sockaddr_in*)result->ai_addr; // Cast addr into AF_INET container
+  raw_addr = &(tmp->sin_addr); // Extract the address from the container
+}
+else { // Address is IPv6
+  struct sockaddr_in6* tmp = (struct sockaddr_in6*)result->ai_addr; // Cast addr into AF_INET6 container
+  raw_addr = &(tmp->sin6_addr); // Extract the address from the container
+}
+inet_ntop(result->ai_family, raw_addr, addr, 100);
+//printf("IPv%d %s\n", result->ai_family == AF_INET6?6:4, addr);
+//result = result->ai_next;
+//}
+//here
+
     //memset(serverMessage,'\0',sizeof(serverMessage));
     //memset(clientMessage,'\0',sizeof(clientMessage));
     if(use_udp == 0) {
-    	socket_c = socket(AF_INET, SOCK_STREAM, 0);
+    	socket_c = socket(result->ai_family, SOCK_STREAM, 0);
     }
     else {
-    	socket_c = socket(AF_INET, SOCK_DGRAM,0);
+    	socket_c = socket(result->ai_family, SOCK_DGRAM,0);
     }
-    server_addr.sin_family = AF_INET;
+    server_addr.sin_family = result->ai_family;
     server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(host);
+    server_addr.sin_addr.s_addr = inet_addr(addr);
     if(use_udp == 0) {
+	//connect(socket_c,result->ai_addr, result->ai_addrlen);
     	connect(socket_c, (struct sockaddr*)&server_addr, sizeof(server_addr));
     //printf("%d\n",serve);
     }
