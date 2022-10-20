f0d166dfe31347cf5913c69c2ccac09a2e23fc79
manaagra <manaagra@iu.edu>
2022-10-20 03:13:42 +0000

    final changes

--- netster/old_file.c
+++ netster/old_file.c
@@ -2,221 +2,221 @@
  *  Here is the starting point for your netster part.1 definitions. Add the
  *  appropriate comment header as defined in the code formatting guidelines
  */
-
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdbool.h>
 #define MAXBYTES 256
 
-void chat_server(char* iface, long port, int use_udp) 
-{
+void chat_server(char *iface, long port, int use_udp) {
   struct addrinfo hints, *result;
-//  char addr[256];
+  //  char addr[256];
   char str[256];
-  sprintf(str,"%ld",port);
+  sprintf(str, "%ld", port);
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
-  if(use_udp == 0) {
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_protocol = IPPROTO_TCP;
- }
-  else {
-        hints.ai_socktype = SOCK_DGRAM;
-        hints.ai_protocol = IPPROTO_UDP;
+  if (use_udp == 0) {
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+  } else {
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
   }
   hints.ai_flags = AI_PASSIVE;
-  getaddrinfo(iface, str, &hints, &result );
-
-if (use_udp == 0) 
-{
-    	int server_file_descr, socket_new;
-  	struct sockaddr_in address;
-  	int addrlength = sizeof(address);
-	server_file_descr = socket(AF_INET, SOCK_STREAM, 0);
-  	address.sin_family = AF_INET;
-  	address.sin_addr.s_addr = INADDR_ANY;
-  	address.sin_port = htons(port);
-  	bind(server_file_descr, (struct sockaddr *)&address, sizeof(address));
-  	listen(server_file_descr, 3);
-    	int connection_count=0;
-    	int flag = 0;
-    	while(1){
-      		socket_new = accept(server_file_descr, (struct sockaddr *)&address, (socklen_t*)&addrlength);
-      		printf("connection %d from ('%s', %d)\n", connection_count, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-      		connection_count+=1;
-    		char buffer[256] = {0};
-      		while(1) {
-        		bzero(buffer, 256);
-        		int val = read( socket_new , buffer, 256);
-			if(val == -1) {printf("error\n");}
-        		printf("got message from ('%s', %d)\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-        		if(strcmp(buffer, "hello\n") == 0) {
-          			send(socket_new , "world\n" , sizeof("world") , 0 );
-        		}
-        		else if (strcmp(buffer, "goodbye\n") == 0){
-          			send(socket_new , "farewell\n" , sizeof("farewell") , 0 );
-          			break;
-        		}
-        		else if (strcmp(buffer, "exit\n") == 0){
-          			send(socket_new , "ok\n" , sizeof("ok") , 0 );
-          			close(server_file_descr);
-          			flag = 1;
-          			break;
-        		}	
-        		else if(strcmp(buffer, "\n") != 0){
-          			send(socket_new , buffer , sizeof(buffer) , 0 );
-        		}
-
-      		}
-      		if(flag) {
-        		break;
-      		}
-    	}
-}
-else {
-    int socket_file_des, flag=0;
-    char buffer[MAXBYTES];
-    struct sockaddr_in servaddr, cliaddr;
-    socket_file_des = socket(AF_INET, SOCK_DGRAM, 0);
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(port);
-
-    bind(socket_file_des, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    int length;
-    length = sizeof(cliaddr);  
+  getaddrinfo(iface, str, &hints, &result);
+  if (use_udp == 0) {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr;
+    int addrlength = sizeof(server_addr);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_socket, 3);
+    int cnt = 0;
+    bool run = false;
     while (1) {
-      bzero(buffer, MAXBYTES);
-      int n_length = recvfrom(socket_file_des, (char *)buffer, MAXBYTES, MSG_WAITALL, (struct sockaddr *) &cliaddr, (unsigned int *)&length);
-      printf("got message from ('%s', %d)\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
-      buffer[n_length] = '\0';
-      if(strcmp(buffer, "hello\n") == 0) {
-        sendto(socket_file_des, "world\n", sizeof("world"),0, (const struct sockaddr *) &cliaddr,length);
+      client_socket = accept(server_socket, (struct sockaddr *)&server_addr,
+                          (socklen_t *)&addrlength);
+      printf("connection %d from ('%s', %d)\n", cnt,
+             inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+      cnt += 1;
+      char buffer[256] = {0};
+      while (1) {
+        bzero(buffer, 256);
+        int val = read(client_socket, buffer, 256);
+        if (val == -1) {
+          printf("error\n");
+        }
+        printf("got message from ('%s', %d)\n", inet_ntoa(server_addr.sin_addr),
+               ntohs(server_addr.sin_port));
+        if (strcmp(buffer, "hello\n") == 0) {
+          send(client_socket, "world\n", sizeof("world"), 0);
+        } else if (strcmp(buffer, "goodbye\n") == 0) {
+          send(client_socket, "farewell\n", sizeof("farewell"), 0);
+          break;
+        } else if (strcmp(buffer, "exit\n") == 0) {
+          send(client_socket, "ok\n", sizeof("ok"), 0);
+          close(server_socket);
+          run = true;
+          break;
+        } else if (strcmp(buffer, "\n") != 0) {
+          send(client_socket, buffer, sizeof(buffer), 0);
+        }
       }
-      else if (strcmp(buffer, "goodbye\n") == 0){
-        sendto(socket_file_des, "farewell\n", sizeof("farewell"),0, (const struct sockaddr *) &cliaddr,length);
-      }
-      else if (strcmp(buffer, "exit\n") == 0){
-        sendto(socket_file_des, "ok\n", sizeof("ok"),0, (const struct sockaddr *) &cliaddr,length);
-        close(socket_file_des);
+      if (run) {
         break;
       }
-      else{
-        sendto(socket_file_des, (const char *)buffer, sizeof(buffer),0, (const struct sockaddr *) &cliaddr,length);
-        }
-      if(flag){
-        close(socket_file_des);
+    }
+  } else {
+    int socket_server, run = 0;
+    char buffer[MAXBYTES];
+    struct sockaddr_in saddr, caddr;
+    socket_server = socket(AF_INET, SOCK_DGRAM, 0);
+
+    memset(&saddr, 0, sizeof(saddr));
+    memset(&caddr, 0, sizeof(caddr));
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(port);
+
+    bind(socket_server, (const struct sockaddr *)&saddr, sizeof(saddr));
+
+    int length;
+    length = sizeof(caddr);
+    while (1) {
+      bzero(buffer, MAXBYTES);
+      int buf_len =
+          recvfrom(socket_server, (char *)buffer, MAXBYTES, MSG_WAITALL,
+                   (struct sockaddr *)&caddr, (unsigned int *)&length);
+      printf("got message from ('%s', %d)\n", inet_ntoa(caddr.sin_addr),
+             ntohs(caddr.sin_port));
+      buffer[buf_len] = '\0';
+      if (strcmp(buffer, "hello\n") == 0) {
+        sendto(socket_server, "world\n", sizeof("world"), 0,
+               (const struct sockaddr *)&caddr, length);
+      } else if (strcmp(buffer, "goodbye\n") == 0) {
+        sendto(socket_server, "farewell\n", sizeof("farewell"), 0,
+               (const struct sockaddr *)&caddr, length);
+      } else if (strcmp(buffer, "exit\n") == 0) {
+        sendto(socket_server, "ok\n", sizeof("ok"), 0,
+               (const struct sockaddr *)&caddr, length);
+        close(socket_server);
+        break;
+      } else {
+        sendto(socket_server, (const char *)buffer, sizeof(buffer), 0,
+               (const struct sockaddr *)&caddr, length);
+      }
+      if (run) {
+        close(socket_server);
       }
     }
-}
+  }
 }
 
-void chat_client(char* iface, long port, int use_udp) {
-	struct addrinfo hints, *result;
-  //char addr[256];
+void chat_client(char *iface, long port, int use_udp) {
+  struct addrinfo hints, *result;
+  // char addr[256];
   char str[256];
-  sprintf(str,"%ld",port);
+  sprintf(str, "%ld", port);
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
-  if(use_udp == 0) {
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_protocol = IPPROTO_TCP;
- }
-  else {
-        hints.ai_socktype = SOCK_DGRAM;
-        hints.ai_protocol = IPPROTO_UDP;
+  if (use_udp == 0) {
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+  } else {
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
   }
   hints.ai_flags = AI_PASSIVE;
-  getaddrinfo(iface, str, &hints, &result );
-  struct addrinfo *ptr;
-  char buf[INET6_ADDRSTRLEN];
-  for ( ptr = result; ptr != NULL; ptr = ptr->ai_next) 
-  {
-    void* raw_addr;
-    if (ptr->ai_family == AF_INET) 
-    { 
-      struct sockaddr_in* tmp = (struct sockaddr_in*)ptr->ai_addr;
-      raw_addr = &(tmp->sin_addr); 
-      if (inet_ntop(ptr->ai_family, raw_addr, buf, INET6_ADDRSTRLEN) == NULL) 
-      {
+  getaddrinfo(iface, str, &hints, &result);
+  struct addrinfo *current;
+  char adr[INET6_ADDRSTRLEN];
+  for (current = result; current != NULL; current = current->ai_next) {
+    void *raw_addr;
+    if (current->ai_family == AF_INET) {
+      struct sockaddr_in *tmp = (struct sockaddr_in *)current->ai_addr;
+      raw_addr = &(tmp->sin_addr);
+      if (inet_ntop(current->ai_family, raw_addr, adr, INET6_ADDRSTRLEN) == NULL) {
         exit(0);
       }
       break;
     }
   }
 
-  int flag=0;
-  if(use_udp == 0) {
+  bool run = false;
+  if (use_udp == 0) {
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[MAXBYTES] = {0};
     sock = socket(AF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
-    serv_addr.sin_addr.s_addr = inet_addr(buf);
+    serv_addr.sin_addr.s_addr = inet_addr(adr);
 
     connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     while (1) {
       bzero(buffer, MAXBYTES);
       int n = 0;
-      while((buffer[n++] = getchar()) != '\n');
-      send(sock , buffer , sizeof(buffer) , 0);
-      if (strcmp(buffer,"goodbye\n")==0){
-		flag=1; 
-	}
-      if(strcmp(buffer,"exit\n")==0){
-		flag=1;
-	}
-      bzero(buffer, MAXBYTES);
-     int val = read( sock , buffer, 256);
-     	if(val == -1) { printf("err\n"); }
-      printf("%s",buffer );
-      if(flag==1)
-          { exit(0);}
-    }
-  }
-  else {
-      int socket_file_des;
-      char buffer[MAXBYTES];
-      struct sockaddr_in servaddr;
-
-      socket_file_des = socket(AF_INET, SOCK_DGRAM, 0);
-      memset(&servaddr, 0, sizeof(servaddr));
-      servaddr.sin_family = AF_INET;
-      servaddr.sin_port = htons(port);
-      servaddr.sin_addr.s_addr = inet_addr(buf);
-      int length,flag=0;
-      while(1){
-        bzero(buffer, MAXBYTES);
-        int n = 0;
-        while((buffer[n++] = getchar()) != '\n');
-        sendto(socket_file_des, (const char *)buffer, sizeof(buffer), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-              if (strcmp(buffer,"goodbye\n")==0){
-		flag = 1;
-	      }
-	if(strcmp(buffer,"exit\n")==0)
-          { 
-		  flag=1;
-          }
-        bzero(buffer, MAXBYTES);
-        recvfrom(socket_file_des, (char *)buffer, MAXBYTES,  MSG_WAITALL, (struct sockaddr *) &servaddr,(unsigned int *) &length);
-        printf("%s", buffer);
-        if (flag==1)
-          { close(socket_file_des);
-            break; }
+      while ((buffer[n++] = getchar()) != '\n');
+      send(sock, buffer, sizeof(buffer), 0);
+      if (strcmp(buffer, "goodbye\n") == 0) {
+        run = true;
       }
+      if (strcmp(buffer, "exit\n") == 0) {
+        run = true;
+      }
+      bzero(buffer, MAXBYTES);
+      int val = read(sock, buffer, 256);
+      if (val == -1) {
+        printf("err\n");
+      }
+      printf("%s", buffer);
+      if (run) {
+        exit(0);
+      }
+    }
+  } else {
+    int socket_server;
+    char buffer[MAXBYTES];
+    struct sockaddr_in saddr;
 
+    socket_server = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+    saddr.sin_addr.s_addr = inet_addr(adr);
+    int length;
+    bool run = false;
+    while (1) {
+      bzero(buffer, MAXBYTES);
+      int n = 0;
+      while ((buffer[n++] = getchar()) != '\n');
+      sendto(socket_server, (const char *)buffer, sizeof(buffer), 0,
+             (const struct sockaddr *)&saddr, sizeof(saddr));
+      if (strcmp(buffer, "goodbye\n") == 0) {
+        run = true;
+      }
+      if (strcmp(buffer, "exit\n") == 0) {
+        run = true;
+      }
+      bzero(buffer, MAXBYTES);
+      recvfrom(socket_server, (char *)buffer, MAXBYTES, MSG_WAITALL,
+               (struct sockaddr *)&saddr, (unsigned int *)&length);
+      printf("%s", buffer);
+      if (run) {
+        close(socket_server);
+        break;
+      }
+    }
   }
 }
 
