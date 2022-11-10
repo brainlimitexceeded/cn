b6cbd29815e6782b126561cdf740ad56163e08e8
Mana Agrawal <manaagrawal@Manas-Air.attlocal.net>
2022-11-10 15:00:20 -0800

    rdt

--- netster/stopandwait.c
+++ netster/stopandwait.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -14,10 +15,15 @@
  *  appropriate comment header as defined in the code formatting guidelines
  */
 /* Add function definitions */
+typedef struct  {
+int sequence;
+int len;
+} header;
 void stopandwait_server(char* iface, long port, FILE* fp) {
     struct addrinfo hints, *result;
-    char str[256];
-    sprintf(str, "%ld", port);
+    header rdt_server;
+    char str[MAXBYTES];
+    // sprintf(str, "%ld", port);
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
@@ -39,21 +45,39 @@ void stopandwait_server(char* iface, long port, FILE* fp) {
     bind(socket_server, (const struct sockaddr *)&saddr, sizeof(saddr));
     int n;
     int length;
+    int sequence_id=-1;
+    bool flag=true;
+    char temp[MAXBYTES-8];
     length = sizeof(caddr);
-    while(1) {
+    bzero(buffer,MAXBYTES);
+    while(flag) {
         n = recvfrom(socket_server,(char *)buffer, sizeof(buffer) , 0, (struct sockaddr *)&caddr, (unsigned int *)&length);
         if(n<=0) {
                 break;
         }
-	    fwrite(buffer,1,n,fp);
-        bzero(buffer,256);
+        memcpy(&rdt_server, buffer, sizeof(rdt_server));
+        if (rdt_server.len < MAXBYTES-8){
+                flag = false;
+        }
+	    //fwrite(buffer,1,n,fp);
+        // bzero(buffer,256);
+        if(sequence_id!=rdt_server.sequence){
+            memcpy(&temp, buffer + sizeof(rdt_server), sizeof(temp));
+            fwrite(temp,1,rdt_server.len,fp);
+            char str[100];
+            sprintf(str, "%d", rdt_server.sequence);
+            sendto(socket_server, str, sizeof(str),0, (const struct sockaddr *)&caddr,length);
+            bzero(buffer,MAXBYTES);
+            sequence_id = rdt_server.sequence;
+        }
+
     }
     close(socket_server);
 }
 
 void stopandwait_client(char* iface, long port, FILE* fp) {
     struct addrinfo hints, *result;
-    char str[256];
+    char str[MAXBYTES];
     sprintf(str, "%ld", port);
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;  
@@ -63,6 +87,7 @@ void stopandwait_client(char* iface, long port, FILE* fp) {
     getaddrinfo(iface, str, &hints, &result);
     struct addrinfo *current;
     char adr[INET6_ADDRSTRLEN];
+    int packet_count = 1;
     for (current = result; current != NULL; current = current->ai_next) {
         void *raw_addr;
         if (current->ai_family == AF_INET) {
@@ -77,18 +102,48 @@ void stopandwait_client(char* iface, long port, FILE* fp) {
     int socket_server;
     char buffer[MAXBYTES];
     struct sockaddr_in saddr;
+    struct timeval t;
+    bool flag = true;
+    // int bufsize;
+    int length, temp_size;
+    char temp[MAXBYTES-8];
 
     socket_server = socket(AF_INET, SOCK_DGRAM, 0);
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(port);
     saddr.sin_addr.s_addr = inet_addr(adr);
-    int bufsize;
-    bzero(buffer,256);
-    while( ( bufsize = fread(buffer,1,256,fp)  )>0 ) {
-        sendto(socket_server,(char *)buffer, bufsize,0,(const struct sockaddr *)&saddr,sizeof(saddr));
+    t.tv_sec = 0;
+    t.tv_usec = 5000;
+    bzero(buffer,MAXBYTES);
+    setsockopt(socket_server, SOL_SOCKET, SO_RCVTIMEO, &t,sizeof(t));
+    while(flag) {
+        temp_size = fread(temp, 1, MAXBYTES-8, fp);
+        header rdtheader;
+        rdtheader.len = temp_size;
+        rdtheader.sequence = packet_count;
+        bzero(buffer, MAXBYTES);
+        memcpy(buffer , &rdtheader , sizeof(rdtheader));
+        memcpy(buffer + sizeof(rdtheader), &temp , rdtheader.len);
+        while (1)
+        {
+            sendto(socket_server, buffer, sizeof(buffer), 0, (const struct sockaddr *)&saddr, sizeof(saddr));
+            recvfrom(socket_server, (char *)buffer, MAXBYTES, MSG_WAITALL, (struct sockaddr *)&saddr, (unsigned int *)&length);
+            if (atoi(buffer) == packet_count)
+            {
+                break;
+            }
+        }
+        if (temp_size < MAXBYTES-8)
+        {
+            flag = false;
+        }
+        packet_count = (packet_count+1)%2;
     }
-    bzero(buffer,256);
-    sendto(socket_server,(char *)buffer, strlen(buffer),0,(const struct sockaddr *)&saddr,sizeof(saddr));
+    // while( ( bufsize = fread(buffer,1,256,fp)  )>0 ) {
+    //     sendto(socket_server,(char *)buffer, bufsize,0,(const struct sockaddr *)&saddr,sizeof(saddr));
+    // }
+    // bzero(buffer,256);
+    // sendto(socket_server,(char *)buffer, strlen(buffer),0,(const struct sockaddr *)&saddr,sizeof(saddr));
     close(socket_server);
 }
